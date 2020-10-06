0909edec2f3663abbcd11a658311cec10dee2f27
Tushar443 <moretushart33@gmail.com>
2020-10-06 16:53:06 +0530

    Assinments

--- JAVA/Q51BankManagement.java
+++ JAVA/Q51BankManagement.java
@@ -1,151 +1,132 @@
 import java.util.Scanner;
+class Customer{
+	double accountNo;
+	String Name;
+	double balance;
+	Customer(double accountNo,String Name,double balance){
+	this.accountNo=accountNo;
+	this.Name=Name;
+	this.balance=balance;
+	}
+	@Override
+	public String toString() {
+	return "Account No " + accountNo + ", Name=" + Name + ", balance=" + balance;
+	}
+	double getaccNo() {
+	return accountNo;
+	}
+	double getbal() {
+	return balance;
+	}
+	void setbal(double balance) {
+	this.balance=balance;
+	}
 
-
-class Bank {
-    private String accType;
-    private int CusAccNumber;
-    private String name;
-    private double balance;
-    private String branch;
-    
-    Bank(){}
-    Bank(String accType,int CusAccNumber,String name,double balance,String branch){
-        this.accType =accType;
-        this.CusAccNumber=CusAccNumber;
-        this.name = name;
-        this.balance =balance;
-        this.branch = branch;
-    }
-
-    public void show(){
-        System.out.println("Account Type : "+ this.accType);
-        System.out.println("Account Number : "+this.CusAccNumber);
-        System.out.println("Name : "+this.name);
-        System.out.println("Balance : "+ this.balance);
-        System.out.println("Branch Name : "+ this.branch);
-    }
 }
-class CurrentAccount extends Bank{
-    
-    CurrentAccount(){}
-        CurrentAccount(int CusAccNumber,String name,double balance,String branch){
-            super("Current Account" ,CusAccNumber, name, balance, branch);
-        }
+class savingAcc extends Customer{
+	savingAcc(double accountNo,String Name,double balance){
+	super(accountNo,Name,balance);
+	}
 }
-class SavingAccount extends Bank{
-    
-    SavingAccount(){}
-        SavingAccount(int CusAccNumber,String name,double balance,String branch){
-            super("Saving Account" ,CusAccNumber, name, balance, branch);
-        }
+class currentAcc extends Customer{
+	currentAcc(double accountNo,String Name,double balance){
+	super(accountNo,Name,balance);
+	}
 }
-public class Q51BankManagement{
-    private static int BankAccount=0;
-    private static Bank arr[] =new Bank[10];
-    private static boolean SavingBalance(double balance){
-        boolean flag = true;
-        if(balance >= 5000){
-            return flag;
-        }else{
-            flag=false;
-        }
-        return flag;
-    }
-    private static boolean CurrentBalance(double balance){
-        boolean flag = true;
-        if(balance >= 1000){
-            return flag;
-        }else{
-            flag=false;
-        }
-        return flag;
-    }
-    private static void CustomerData(Bank s){
-        for(int i =0;i<arr.length;i++){
-            if(arr[i]==null){
-                arr[i]=s;
-            }else{
-                continue;
-            }
-        }
-    }
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        /**  Bank b= new SavingAccount(123,"Tushar",300.5,"Kurla");
-        // Bank b1= new SavingAccount(124,"pravin",5467.5,"nasik");
-        // Bank b2= new CurrentAccount(125,"rohit",12346.5,"satara");
-        // Bank b3= new CurrentAccount(126,"Kishore",7896.5,"igatpuri");
-        // b.show();
-        // b1.show();
-        // b2.show();
-        // b3.show();
-        */
-        boolean flag = true;
-        while(flag){
-            System.out.println("!!!!!! Bank Management !!!!!!");
-            System.out.println("###################################");
-            System.out.println(
-             "1 : Create new account\n"
-            +"2 : Display Account Details\n"
-            +"3 : Money Details\n" 
-            +"4 : To Deposit and Withdrawal\n");
-            System.out.println("###################################");
-            System.out.print("Enter your choice : ");
-            int n =sc.nextInt();
-            switch(n){
-                case 1:
-                    System.out.println("1: For Saving Account\n"+
-                                       "2: For Current Acount\n");
-                    System.out.print("Enter your choice : ");
-                    int x =sc.nextInt();
-                    if(x ==1){
-                        BankAccount++;
-                        System.out.print("Enter Name :");
-                        String name =sc.next();
-                        System.out.print("Enter Balance :");
-                        double balance =sc.nextDouble();
-                        if(SavingBalance(balance)){
+public class Q51 {
 
-                        }else{
-                            System.out.println("Balance Should be more than 5000");
-                            break;
-                        }
-                        System.out.print("Enter Branch Name : ");
-                        String branch =sc.next();
-                        Bank s =new SavingAccount(BankAccount,name,balance,branch);
-                        CustomerData(s);
-                        s.show();
-                    }else if (x ==2){
-                        BankAccount++;
-                        System.out.print("Enter Name :");
-                        String name =sc.next();
-                        System.out.print("Enter Balance :");
-                        double balance =sc.nextDouble();
-                        if(CurrentBalance(balance)){
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	Customer c[]= new Customer[2];
 
-                        }else{
-                            System.out.println("Balance Should be more than 1000");
-                            break;
-                        }
-                        System.out.print("Enter Branch Name : ");
-                        String branch =sc.next();
-                        Bank s =new CurrentAccount(BankAccount,name,balance,branch);
-                        CustomerData(s);
-                        s.show();
-                    }else{
-                        System.out.println("Invalid Choice");
-                    }
-                    break; 
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    System.out.println("Invalid Choice");
-                    break;
-            }
-        }
-    }
+	int i=0;
+	while(true) {
+	System.out.println("Enter Your Choice");
+	System.out.println("1.Add Saving Account Details\n2.Add Current Account Details\n"
+	+ "3.Enter Amount in Account\n4.withdraw Amount from Account\n5.Display Information Of ACcounts\n6.Display Information Of Particular ACcounts\n7.Exit");
+	int ch=sc.nextInt();
+	switch(ch) {
+	case 1:{
+	System.out.println("Enter Saving Account details");
+	double acc=sc.nextDouble();
+	String name=sc.next();
+	double bal=sc.nextDouble();
+	
+	c[i]=new savingAcc(acc,name,bal);
+	i++;}
+	break;
+	case 2:{
+	System.out.println("Enter Current Account details");
+	double acc=sc.nextDouble();
+
+	String name=sc.next();
+	double bal=sc.nextDouble();
+	
+	c[i]=new currentAcc(acc,name,bal);
+	i++;}
+	break;
+	case 3:
+	System.out.println("Enter Account Number");
+	double k2=sc.nextDouble();
+	for(Customer cc: c) {
+	if(cc.getaccNo()== k2) {
+	System.out.println("Enter Amount You Want to deposite");
+	double k3=sc.nextDouble();
+	double b=cc.getbal()+k3;
+	cc.setbal(b);
+	}
+	}
+	case 4:
+	System.out.println("Enter Account Number");
+	double k4=sc.nextDouble();
+	for(Customer cc: c) {
+	if(cc.getaccNo()== k4) {
+	System.out.println("Enter Amount You Want to withdraw");
+	double k3=sc.nextDouble();
+	if(cc instanceof savingAcc) {
+	if(cc.getbal()>5000 && cc.getbal()> (5000+k3)) {
+	double b=cc.getbal()-k3;
+	cc.setbal(b);
+	}
+	else {
+	System.out.println("Minimum Balance Is low.Saving Account Minimum balance should be 5000");
+	}}
+	else if(cc instanceof currentAcc) {
+	if(cc.getbal()>1000 && cc.getbal()> (1000+k3)) {
+	double b=cc.getbal()-k3;
+	cc.setbal(b);
+	}
+	else {
+	System.out.println("Minimum Balance Is low.Current Account Minimum balance should be 1000");
+	}
+	}
+	}
+	}
+	break;
+	case 5:
+	for(Customer cc:c) {
+	if(cc instanceof savingAcc) {
+	System.out.println("Saving Account DEtails");
+	System.out.println(cc.toString());
+	}
+	else if(cc instanceof currentAcc) {
+	System.out.println("Current Account DEtails");
+	System.out.println(cc.toString());
+	}
+	}
+	break;
+	case 6:System.out.println("Enter Accout number:\n");{
+	double k1=sc.nextDouble();
+	for(Customer cc:c) {
+	if(cc.getaccNo()==k1) {
+	System.out.println(cc.toString());
+	}
+	}}
+	break;
+
+	case 7 :return;
+	}
+	}
+
+}
 }
